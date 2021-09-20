53369ad558d864051735f2ab2731a8cafdac4a33
ay <aasmaanyadav1@gmail.com>
2021-09-20 13:10:19 +0100

    created initial test models

---
+++ melanoma_test/melanoma_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  melanoma_testApp.swift
+//  melanoma_test
+//
+//  Created by Yadav, Aasmaan (SPH) on 20/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct melanoma_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
